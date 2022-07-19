5cbfa83b818f910ffbe54ce889c633f49ea387c3
matias179 <mati_onngames@hotmail.com>
2022-07-19 20:09:07 -0300

    se agrega nuevo proyecto

---
+++ variablesjv.java
@@ -0,0 +1,30 @@
+alert("Hola, estas en mi web");
+
+const variablenombre = "Matias Leal";
+const variableEdad = 22;
+const arraynum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const boolean = true;
+const arraynombres = ["matias", "debora", "william", "rodrigo", "fran"];
+
+
+console.log(variablenombre);
+console.log(variableEdad);
+console.log(boolean);
+console.log(arraynum);
+console.log(arraynombres)
+
+
+const objetodatos = {
+    name: "Matias Leal",
+    edad: 22,
+    numarray: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,],
+    buleano: true,
+    nombresarray: ["matias", "debora", "william", "rodrigo", "fran"],
+}
+
+console.log(objetodatos);
+
+
+
+
+
